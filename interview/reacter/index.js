4c5de0c007e8f7cb47e96938a4562b11346d37b0
sharath kumar <sharathkumar@sharaths-MacBook-Air.local>
2023-06-10 22:16:19 +0530

    moved react code to other project

--- interview/reacter/index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './src/App'
-
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<App tab='home' />)
