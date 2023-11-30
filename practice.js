dd7dd07aef0c4118884f9b106cf49220d73f5413
sharath <sharathdupati@gmail.com>
2023-11-30 22:04:05 +0530

    practice

--- practice.js
+++ practice.js
@@ -1,25 +1,39 @@
-function debounce (fun, delay = 3000) {
-  let timerId
+function debounce(fun, delay = 3000) {
+  let timerId;
   return function () {
-    const context = this
-    const args = arguments
-    clearTimeout(timerId)
+    const context = this;
+    const args = arguments;
+    clearTimeout(timerId);
     timerId = setTimeout(() => {
-      fun.apply(context, args)
-    }, [delay])
-  }
+      fun.apply(context, args);
+    }, [delay]);
+  };
 }
 
-function throttle (fun, delay = 3000) {
-  let fired = false
+function throttle(fun, delay = 3000) {
+  let fired = false;
   return function () {
-    if (fired) return
-    const context = this
-    const args = arguments
-    fun.apply(context, args)
-    fired = true
+    if (fired) return;
+    const context = this;
+    const args = arguments;
+    fun.apply(context, args);
+    fired = true;
     setTimeout(() => {
-      fired = false
-    }, [delay])
-  }
+      fired = false;
+    }, [delay]);
+  };
 }
+
+Function.prototype.myBind = function () {
+  const fun = this;
+  const [context, ...args1] = arguments;
+  return function () {
+    fun.apply(context, [...args1, ...arguments]);
+  };
+};
+
+const helloWorld = function (a, b, c) {
+  console.log("hello", a, b, c, this);
+};
+const binded = helloWorld.myBind({ a: 1 }, 1, 3);
+binded(2);
