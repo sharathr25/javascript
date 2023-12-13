5651a1e91c4ad4175c456c4e1868ed0297636552
sharath <sharathdupati@gmail.com>
2023-12-13 13:59:12 +0530

    jumpgame2

---
+++ algorithms/topInterview150/medium/jumpGame2.js
@@ -0,0 +1,51 @@
+/**
+You are given a 0-indexed array of integers nums of length n. 
+You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. 
+In other words, if you are at nums[i], 
+you can jump to any nums[i + j] where: 0 <= j <= nums[i] and i + j < n
+
+Return the minimum number of jumps to reach nums[n - 1]. 
+The test cases are generated such that you can reach nums[n - 1].
+
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. 
+Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+Constraints:
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+It's guaranteed that you can reach nums[n - 1].
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  const n = nums.length - 1;
+  const memo = {};
+
+  const fun = (index) => {
+    if (index in memo) return memo[index];
+    if (index === n) return 0;
+
+    let min = Infinity;
+    for (let i = 1; i <= nums[index] && i <= n; i++) {
+      min = Math.min(1 + fun(index + i), min);
+    }
+
+    memo[index] = min;
+    return min;
+  };
+
+  return fun(0, 0);
+};
